539a2120093d07ffd323ca4adeb0202ae07a2e5c
reaper <reaper>
2023-02-23 08:53:18 +0300

    Созданние всех функций

---
+++ Homework.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Homework {
+    public static void main(String args[]) {
+        int[] sortArr = {12, 6, 4, 1, 15, 10};
+        int[] result = mergeSort(sortArr);
+        System.out.println(Arrays.toString(result));
+    }
+    public static int[] mergeSort(int[] sortArr) {
+    }
+
+    public static int[] mergeSortInner(int[] buffer1, int[] buffer2, int startIndex, int endIndex) {
+        }
+    }
+}
